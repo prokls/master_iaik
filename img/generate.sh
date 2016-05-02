13dcf0a8b80300efb6be5eeff5895b78dfa82be5
prokls <lukas.prokop@student.tugraz.at>
2016-05-02 03:42:48 +0200

    General development. Introduction production-ready.

--- img/generate.sh
+++ img/generate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
 for svg in ./*.svg;
-  do inkscape -D -z --file="$svg" --export-pdf="${svg:r}".pdf --export-latex --export-text-to-path
+  do
+    #inkscape -D -z --file="$svg" --export-pdf="${svg:r}".pdf --export-latex --export-text-to-path
+    inkscape -D -z --export-pdf="${svg:r}.pdf" --export-pdf-version=1.5 "$svg";
 done
